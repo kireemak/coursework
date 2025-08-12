675a8116870c5c983293cce7e4e939874ecf6bb2
Kireenko Makar <kireenko_makar@mail.ru>
2025-08-12 18:09:34 +0300

    Настройка CORS и исправление ошибок.

--- src/main/java/by/kireenko/coursework/CarBooking/services/CustomUserDetailsService.java
+++ src/main/java/by/kireenko/coursework/CarBooking/services/CustomUserDetailsService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -46,7 +45,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         user.setPassword(registrationUserDto.getPassword());
         user.setEmail(registrationUserDto.getEmail());
         user.setPhoneNumber(registrationUserDto.getPhone());
-        user.setRoles(Set.of(roleService.getUserRole()));
+        user.setRoles(List.of(roleService.getUserRole()));
         return userService.createUser(user);
     }
 
@@ -56,7 +55,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         user.setPassword(registrationUserDto.getPassword());
         user.setEmail(registrationUserDto.getEmail());
         user.setPhoneNumber(registrationUserDto.getPhone());
-        user.setRoles(Set.of(roleService.getAdminRole()));
+        user.setRoles(List.of(roleService.getAdminRole()));
         return userService.createUser(user);
     }
 }
